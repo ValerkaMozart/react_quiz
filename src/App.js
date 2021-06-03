f4a37b3634f02d7cd9ba1ec9658e0ca0d7e82574
ValerkaMozart <valerkamozart@mail.ru>
2021-06-03 16:53:24 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Layout from "./hoc/layout/Layout";
+import {Switch, Route, Redirect, withRouter} from 'react-router-dom'
+import Quiz from "./Containers/Quiz/Quiz";
+import QuizCreator from "./Containers/QuizCreator/QuizCreator"
+import QuizList from "./Containers/QuizList/QuizList"
+import Auth from "./Containers/Auth/Auth";
+import Logout from "./Components/Logout/Logout";
+import {connect} from "react-redux";
+import {autoLogin} from "./store/actions/auth";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App  extends React.Component{
+    componentDidMount() {
+        this.props.autoLogin()
+    }
+
+    render () {
+       let routes = (
+            <Switch>
+                <Route path="/auth" component={Auth}/>
+                <Route path="/quiz/:id" component={Quiz}/>
+                <Route path="/" exact component={QuizList}/>
+                <Redirect to={'/'}/>
+            </Switch>
+        )
+
+        if (this.props.isAuthenticated) {
+            routes = (
+                <Switch>
+                    <Route path="/quiz-creator" component={QuizCreator}/>
+                    <Route path="/quiz/:id" component={Quiz}/>
+                    <Route path={'/logout'} component={Logout}/>
+                    <Route path="/" exact component={QuizList}/>
+                    <Redirect to={'/'}/>
+                </Switch>
+            )
+        }
+
+        return (
+            <Layout>
+                { routes }
+            </Layout>
+        )
+    }
+
+
+}
+function mapStateToProps(state) {
+    return {
+        isAuthenticated: !!state.auth.token
+    }
+}
+function mapDispatchToProps(dispatch) {
+    return {
+        autoLogin: () => dispatch(autoLogin())
+    }
 }
 
-export default App;
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App))
--- src/index.js
+++ src/index.js
@@ -3,12 +3,44 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from 'react-router-dom'
+import {createStore, compose, applyMiddleware} from 'redux'
+import {Provider} from 'react-redux'
+import rootReducer from "./store/reducers/rootReducer";
+import thunk from "redux-thunk";
+
+let loggerMiddleWare = state => next => action => {
+    let result = next(action)
+
+    return result
+}
+
+const composeEnhancers =
+    typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+            // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+        }) : compose;
+
+
+
+let store = createStore(
+    rootReducer,
+    composeEnhancers(
+        applyMiddleware(thunk, loggerMiddleWare)
+    )
+)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <BrowserRouter>
+                <App/>
+            </BrowserRouter>
+        </Provider>
+
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
